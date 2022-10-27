0462a95e128d4bf06a1d3d8e2408a97784b0ac81
PotatoHD404 <31506993+PotatoHD404@users.noreply.github.com>
2022-10-27 10:49:21 +0300

    improved testing

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
 import exrex
 import re
 
+a = r'^(ab*)(ab*)*|([ba])*b$'
+b = r'^a([ab])*|b|b([ab]*b)$'
+
+
+def main():
+    i = 0
+    for test in exrex.generate(b, 6):
+        if re.match(a, test) is None:
+            print(test)
+            print('Not matched')
+        i += 1
+        if i == 1000000:
+            break
+    print(i)
+
 
 # def main():
 #     i = 0
-#     for test in exrex.generate(r'(a|b*)(ab)*|([ab])*b', 10):
-#         if re.match(r'^a(b*a+b)*b*|b(b*a+b)*b*|$', test) is None:
+#     for test in exrex.generate(r'b[a|b]*b|b|a(a|b[a|b]*)', 10):
+#         if re.match(r'^(ab*)(ab*)*|([b|a])*b$', test) is None:
 #             print(test)
 #             print('Not matched')
 #         i += 1
 #     print(i)
 
-
-def main():
-    i = 0
-    for test in exrex.generate(r'a(b*a+b)*b*|b(b*a+b)*b*|', 5):
-        if re.match(r'^(a|b*)(ab)*|([ab])*b$', test) is None:
-            print(test)
-            print('Not matched')
-        i += 1
-    print(i)
-
-
+h
 if __name__ == '__main__':
     main()
